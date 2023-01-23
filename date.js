fea3b7392bd07560771aa3ff9d24b7295b5c44d2
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-01-23 22:36:49 +0300

    updated code

---
+++ date.js
@@ -0,0 +1,9 @@
+let date_1 = new Date('10/25/2023');
+let date_2 = new Date();
+
+const days = (date_1, date_2) =>{
+    let difference = date_1.getTime() - date_2.getTime();
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+}
+console.log(days(date_1, date_2) +" days to world cup");
